bf319af8de8d4bec00698b6701e0639c58aecf38
ivajotha <miavrain@gmail.com>
2016-07-31 01:48:16 -0500

    Add Layout Preview Info

--- app/src/main/java/mx/ivajotha/space/MainActivity.java
+++ app/src/main/java/mx/ivajotha/space/MainActivity.java
@@ -2,7 +2,15 @@ package mx.ivajotha.space;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.Date;
 
 import mx.ivajotha.space.data.ApodService;
 import mx.ivajotha.space.helper.Data;
@@ -13,20 +21,30 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView dateSingle;
+    private TextView explanationSingle;
+    private TextView titleSingle;
+    private ImageView urlImgSingle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dateSingle = (TextView) findViewById(R.id.dateSingle);
+        explanationSingle = (TextView) findViewById(R.id.explanationSingle);
+        titleSingle = (TextView) findViewById(R.id.titleSingle);
+        urlImgSingle = (ImageView) findViewById(R.id.imageSingle);
+
         //set Instance Apo
         ApodService apodService = Data.getInstance().create(ApodService.class);
 
         //Declare variable for response Apod
-
         //Metodos de ApodService
         //Call<Apod> callApodService = apodService.getTodayApod();
         String apiKey = "5Njm32H3YhmhIkWBJxNpXAReRHJdoXLi4hD4pBvw";
         Call<Apod> callApodService = apodService.getTodayApodWithQuery(apiKey);
+
 
         //Callback de Apod
         callApodService.enqueue(new Callback<Apod>() {
@@ -34,6 +52,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<Apod> call, Response<Apod> response) {
                 //Log.d("APOD", response.body().getTitle());
+                dateSingle.setText(response.body().getDate());
+                explanationSingle.setText(response.body().getExplanation());
+                titleSingle.setText(response.body().getTitle());
+                String urlImg = response.body().getUrl();
+
+                Picasso.with(getApplicationContext()).load(urlImg).into(urlImgSingle);
             }
 
             @Override
