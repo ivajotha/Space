3189e44a4136d51673f6c544fe252a3037e6aeed
ivajotha <miavrain@gmail.com>
2016-08-12 19:33:11 -0500

    Add Menu

--- app/src/main/java/mx/ivajotha/space/MainActivity.java
+++ app/src/main/java/mx/ivajotha/space/MainActivity.java
@@ -1,13 +1,20 @@
 package mx.ivajotha.space;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -31,16 +38,60 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+    /*
     @BindView(R.id.mars_rover_listing)
     RecyclerView recyclerView;
+   */
+
+    @BindView(R.id.listing_toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.listing_navigation_drawer)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.listing_navigation_view)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.listing_navigation_activity);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
+                switch (item.getItemId()){
+                    case R.id.mars_apod_item:
+                        Snackbar.make(findViewById(android.R.id.content),"HOLA",Snackbar.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+        ActionBarDrawerToggle actionbarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout ,toolbar,R.string.app_name, R.string.app_name){
+            @Override
+            public void onDrawerClosed(View drawerView)
+            {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        drawerLayout.addDrawerListener(actionbarDrawerToggle);
+        actionbarDrawerToggle.syncState();
+
+        /*
         //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
         recyclerView.setLayoutManager(gridLayoutManager);
@@ -75,6 +126,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        */
 
     }
 }
